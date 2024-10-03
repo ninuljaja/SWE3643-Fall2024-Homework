21302caa567a484d30badeb7c27693ef7865d727
nino <93230927+ninuljaja@users.noreply.github.com>
2024-10-03 10:43:24 -0400

    fix generated test

--- Homework3/src/test/java/org/example/GeneratedTest.java
+++ Homework3/src/test/java/org/example/GeneratedTest.java
@@ -6,6 +6,8 @@ import com.microsoft.playwright.options.*;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @UsePlaywright
 public class GeneratedTest {
     @Test
@@ -17,4 +19,17 @@ public class GeneratedTest {
         page.getByLabel("Toggle Todo").check();
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Clear completed")).click();
     }
+
+    @Test
+    void test2(Page page) {
+        page.navigate("https://demo.playwright.dev/todomvc/");
+        page.navigate("https://demo.playwright.dev/todomvc/#/");
+        page.getByPlaceholder("What needs to be done?").click();
+        page.getByPlaceholder("What needs to be done?").fill("Complete homework 3");
+        page.getByPlaceholder("What needs to be done?").press("Enter");
+        page.getByLabel("Toggle Todo").check();
+        String itemsLeftText = page.locator(".todo-count").textContent();
+        assertTrue(itemsLeftText.contains("0 items left"));
+    }
+
 }
